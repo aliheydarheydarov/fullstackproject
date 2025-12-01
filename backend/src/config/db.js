88fe7761fc15d16bcba3a64aacacb9a684b781cf
aliheydarheydarov <137005463+aliheydarheydarov@users.noreply.github.com>
2025-12-01 13:36:30 +0400

    Modify MongoDB connection string
    Updated MongoDB connection string to use a placeholder.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://eliheyderheyderov1:asdasfamanas@cluster0.hbz8x4q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    `<mongodb_srv>`
   )
   .then(() => {
     console.log("connected to db");
